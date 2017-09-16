874e4a96afa89e315ea85723987a2ffb7372c9fc
yuzengzeng <13259745029@163.com>
2017-09-16 12:03:19 +0800

    加入了创建数据库和表的各项配置。

---
+++ app/src/main/java/com/vivo/yzz/myweather/db/City.java
@@ -0,0 +1,49 @@
+package com.vivo.yzz.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/9/16.
+ */
+
+public class City extends DataSupport {
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+}
---
+++ app/src/main/java/com/vivo/yzz/myweather/db/County.java
@@ -0,0 +1,49 @@
+package com.vivo.yzz.myweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/9/16.
+ */
+
+public class County extends DataSupport {
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+}
